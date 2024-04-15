170be28f242506c169501a4acdfe797d405c0beb
Jimbo Jones <128925976+jsteele15@users.noreply.github.com>
2024-04-15 14:13:02 +0100

    v2
    - added zombies

--- src/main.js
+++ src/main.js
@@ -4,9 +4,18 @@ import Game from './scene/game.js'
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
-    width: 480,
+    width: 640,
     height: 640,
-    scene: Game
+    scene: Game,
+    physics: {
+        default: 'arcade',
+        arcade:{
+            gravity: {
+                y: 200
+            },
+            debug: true
+        }
+    }
 })
 
 console.dir(Phaser)
--- src/scene/game.js
+++ src/scene/game.js
@@ -1,5 +1,19 @@
 import Phaser from '../lib/phaser.js'
 
+//this function creates a hord
+const create_h = function(zombie, z_string){
+    for (let i = 0; i < 5; i++){
+        const x = Phaser.Math.Between(80, 400)
+        const y = 150*i
+
+        const b_z = zombie.create(x, y, z_string)
+        b_z.scale = 0.2
+
+        const body = b_z.body
+        body.updateFromGameObject()
+    }
+}
+
 export default class Game extends Phaser.Scene{
     constructor()
     {
@@ -8,11 +22,23 @@ export default class Game extends Phaser.Scene{
 
     preload()
     {
-        this.load.image('background', '../res/test.jpg')
+        this.load.image('base_zombie', '../res/test.jpg')
     }
 
     create()
     {
-        this.add.image(240, 320, 'background')
+        const base_zombie = this.physics.add.staticGroup()
+
+        create_h(base_zombie, 'base_zombie')
+        //for (let i = 0; i < 5; i++){
+            //const x = Phaser.Math.Between(80, 400)
+            //const y = 150*i
+
+            //const b_z = base_zombie.create(x, y, 'base_zombie')
+            //b_z.scale = 0.5
+
+            //const body = b_z.body
+            //body.updateFromGameObject()
+        //}
     }
 }
